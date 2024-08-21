04ea172972dda8ccd31d57dffc53ffc452117900
sammyowase <samuelowase02@gmail.com>
2024-08-21 22:46:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+//  var firstName = prompt("Enter First Name: ")
+//  var lastName = prompt("Enter Last Name: ")
+//  var age = prompt("Enter Age: ")
+//  var Height = prompt("What's your height?")
+//  var petName = prompt("What is your pet's name? ") ;
+//  alert("Thanks for the information.");
+
+//  var nameCond = null
+//  var ageCond = null
+//  var heightCond = null
+//  var petCond = null;
+
+//  if (firstName[0] === lastName[0]) {
+//     nameCond = true
+//  }
+//  else{
+//     nameCond = false
+//  }
+
+//  if (age > 20 && age <= 25 ) {
+//     ageCond = true
+//  }
+//  else{
+//     age = false
+//  }
+
+//  if (Height >= 170) {
+//     heightCond = true
+//  }
+//  else {
+//     heightCond = false
+//  }
+
+//  if (petName[petName.length-1] === "y") {
+//     petCond = true
+//  }
+//  else{
+//     petCond = false
+//  }
+
+//  if (nameCond && ageCond && heightCond && petCond) {
+//     console.log("WELCOME SPY!");
+    
+//  }
+//  else{
+//     console.log("Nothing to see here!!");
+    
+//  }
+
+
+// let a = parseInt(prompt("Enter a value: "));
+// let b = parseInt(prompt("Enter another value: "));
+// let highest = Math.max (a, b)
+// alert(
+//     highest + " is the highest Number"
+// )
